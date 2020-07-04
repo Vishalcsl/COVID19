356a554f7c890898512596a066b5cff5c7d78663
Vishalcsl <vishalgautamcsl@gmail.com>
2020-07-04 16:13:07 +0530

    added build file

---
+++ client/build/precache-manifest.f74d4c01aeb0afd40ee8511f5285cfed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1651fdb00090cc725d1e311991401150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "755b4caede71fcfd999c",
+    "url": "/static/css/2.e9f65d93.chunk.css"
+  },
+  {
+    "revision": "f85fafe3daeddec0648a",
+    "url": "/static/css/main.0bfdaaf0.chunk.css"
+  },
+  {
+    "revision": "755b4caede71fcfd999c",
+    "url": "/static/js/2.1cc6c2c2.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.1cc6c2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85fafe3daeddec0648a",
+    "url": "/static/js/main.85a78d08.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
